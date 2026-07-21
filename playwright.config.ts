28344f78828a959cd676d63fc84c776eb702de7f
e-shn <ellie.shevlin@kainos.com>
2026-07-21 19:36:19 +0100

    Set workers to 1

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
   testDir: './tests',
+  workers: 1,
   reporter: [['html']],
   use: {
     baseURL: process.env.BASE_URL || 'http://localhost:3000',
